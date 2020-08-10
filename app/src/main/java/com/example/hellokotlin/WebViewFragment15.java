b8bea688c87c11f97e2b1992c5b9869cbdcd597f
jurovision <elvofjustice@gmail.com>
2020-08-10 18:48:48 +0200

    Webview erfolgreich umgesetzt 2

---
+++ app/src/main/java/com/example/hellokotlin/WebViewFragment15.java
@@ -0,0 +1,26 @@
+package com.example.hellokotlin;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.fragment.app.Fragment;
+
+
+public class WebViewFragment15 extends Fragment {
+    private WebView webView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_web_view_fragment15, container, false);
+        webView = (WebView) view.findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://play.kotlinlang.org");
+        return view;
+    }
+
+}
